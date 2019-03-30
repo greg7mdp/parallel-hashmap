e5c4e46bd5d9ebbd99fae4c27f61723042fd5908
greg <greg7mdp@gmail.com>
2019-03-30 14:10:57 -0400

    add couple examples

---
+++ examples/hash_std.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+#include "hash_std.h"   // defines Person  with std::hash specialization
+
+#include <parallel_hashmap/phmap.h>
+
+int main()
+{
+    // As we have defined a specialization of std::hash() for Person,
+    // we can now create sparse_hash_set or sparse_hash_map of Persons
+    // ----------------------------------------------------------------
+    phmap::flat_hash_set<Person> persons = 
+        { { "John", "Galt" },
+          { "Jane", "Doe" }
+        };
+
+    for (auto& p: persons)
+        std::cout << p._first << ' ' << p._last << '\n';
+
+}
---
+++ examples/hash_std.h
@@ -0,0 +1,36 @@
+#ifndef phmap_example_hash_std_
+#define phmap_example_hash_std_
+
+#include <parallel_hashmap/phmap_utils.h>
+
+using std::string;
+
+struct Person
+{
+    bool operator==(const Person &o) const
+    { 
+        return _first == o._first && _last == o._last; 
+    }
+
+    string _first;
+    string _last;
+};
+
+namespace std
+{
+// inject specialization of std::hash for Person into namespace std
+// ----------------------------------------------------------------
+    template<>
+    struct hash<Person>
+    {
+        std::size_t operator()(Person const &p) const
+        {
+            std::size_t seed = 0;
+            phmap::hash_combine(seed, p._first);
+            phmap::hash_combine(seed, p._last);
+            return seed;
+        }
+    };
+}
+
+#endif // phmap_example_hash_std_
