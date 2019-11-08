787011e610c286a165d19b839f691c37b6ce227a
greg <greg7mdp@gmail.com>
2019-11-07 21:43:37 -0500

    fix warning with vs2019 - issue #35

--- parallel_hashmap/phmap_base.h
+++ parallel_hashmap/phmap_base.h
@@ -447,15 +447,6 @@ using enable_if_t = typename std::enable_if<B, T>::type;
 template <bool B, typename T, typename F>
 using conditional_t = typename std::conditional<B, T, F>::type;
 
-template <typename... T>
-using common_type_t = typename std::common_type<T...>::type;
-
-template <typename T>
-using underlying_type_t = typename std::underlying_type<T>::type;
-
-template <typename T>
-using result_of_t = typename std::result_of<T>::type;
-
 namespace type_traits_internal {
 
 // ----------------------------------------------------------------------
