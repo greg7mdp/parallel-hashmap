1a0fdbf3f317fbe0519ff6ac986de3185b4dbcfa
greg <greg7mdp@gmail.com>
2020-03-28 14:24:22 -0400

    set visual studio flags to accurately report and use latest c++ features.

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -26,7 +26,7 @@ function(phmap_cc_test)
   )
 if(MSVC)
   target_compile_options(${_NAME}
-    PRIVATE ${PHMAP_CC_TEST_CWOPTS} /W4
+    PRIVATE ${PHMAP_CC_TEST_CWOPTS} /W4 /Zc:__cplusplus /std:c++latest
   )
 else()
   target_compile_options(${_NAME}
