535ea237419b2314070e0f79bd7e15554708d876
greg <greg7mdp@gmail.com>
2020-10-02 14:08:27 -0400

    issue #67

--- examples/basic.cc
+++ examples/basic.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#define PHMAP_BIDIRECTIONAL 1
 #include <parallel_hashmap/phmap.h>
 
 using phmap::flat_hash_map;
+using namespace std; 
  
 int main()
 {
@@ -18,6 +20,20 @@ int main()
  
     // and print it
     std::cout << "bill's email is: " << email["bill"] << "\n";
+
+    phmap::flat_hash_set<int> v1 { 1, 2, 3, 4, 5 };
+
+    for (auto x : v1)
+        cout << (x) << " "; 
+
+    auto i1 = v1.end();
+    for ( ;i1 != v1.begin(); --i1) 
+    { 
+        if (i1 != v1.end()) 
+            cout << (*i1) << " "; 
+    } 
+    if (i1 != v1.end())
+        cout << (*i1); 
  
     return 0;
 }
