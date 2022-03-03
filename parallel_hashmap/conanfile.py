cfacd2602335fd7570e32baeb3325a39f01a4c63
greg <greg7mdp@gmail.com>
2022-03-03 13:56:40 -0500

    Update conanfile.py

--- parallel_hashmap/conanfile.py
+++ parallel_hashmap/conanfile.py
@@ -6,8 +6,9 @@ import os
 
 class SparseppConan(ConanFile):
     name = "parallel_hashmap"
-    version = "1.27"
+    version = "1.34"
     description = "A header-only, very fast and memory-friendly hash map"
+    url = "https://github.com/greg7mdp/parallel-hashmap/blob/master/parallel_hashmap/conanfile.py"
     
     # Indicates License type of the packaged library
     license = "https://github.com/greg7mdp/parallel-hashmap/blob/master/LICENSE"
