d62b8472a305359ced0417dd9e852e4488afb2dc
greg7mdp <greg7mdp@gmail.com>
2022-07-25 22:57:43 -0400

    Add doc for parallel hashmap extended APIs, and remove some unnecessary casts in phmap.hpp

--- parallel_hashmap/phmap.h
+++ parallel_hashmap/phmap.h
@@ -3322,7 +3322,7 @@ public:
     template <class F>
     void for_each_m(F&& fCallback) {
         for (auto& inner : sets_) {
-            typename Lockable::UniqueLock m(const_cast<Inner&>(inner));
+            typename Lockable::UniqueLock m(inner);
             std::for_each(inner.set_.begin(), inner.set_.end(), fCallback);
         }
     }
@@ -3344,7 +3344,7 @@ public:
         std::for_each(
             std::forward<ExecutionPolicy>(policy), sets_.begin(), sets_.end(),
             [&](auto& inner) {
-                typename Lockable::UniqueLock m(const_cast<Inner&>(inner));
+                typename Lockable::UniqueLock m(inner);
                 std::for_each(inner.set_.begin(), inner.set_.end(), fCallback);
             }
         );
@@ -3365,10 +3365,10 @@ public:
     }
 
     template <class F>
-    void with_submap_m(size_t idx, F&& fCallback) const {
+    void with_submap_m(size_t idx, F&& fCallback) {
         Inner& inner   = sets_[idx];
         auto&  set     = inner.set_;
-        typename Lockable::UniqueLock m(const_cast<Inner&>(inner));
+        typename Lockable::UniqueLock m(inner);
         fCallback(set);
     }
 
