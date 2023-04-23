002f58edd1a7f2ebd6fea3226818bc479b4a53d3
greg7mdp <greg7mdp@gmail.com>
2023-04-23 19:51:43 -0400

    Fix another clang-tidy warning

--- parallel_hashmap/phmap.h
+++ parallel_hashmap/phmap.h
@@ -1193,7 +1193,7 @@ public:
         // Hash, equality and allocator are copied instead of moved because
         // `that` must be left valid. If Hash is std::function<Key>, moving it
         // would create a nullptr functor that cannot be called.
-        settings_(that.settings_) {
+        settings_(std::move(that.settings_)) {
         // growth_left was copied above, reset the one from `that`.
         that.growth_left() = 0;
     }
