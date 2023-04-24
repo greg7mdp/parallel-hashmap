4fe3c8d5022a91cad860b773429dda654d767da8
greg7mdp <greg7mdp@gmail.com>
2023-04-23 23:38:35 -0400

    Bump version to 1.3.10

--- parallel_hashmap/phmap_config.h
+++ parallel_hashmap/phmap_config.h
@@ -36,7 +36,7 @@
 
 #define PHMAP_VERSION_MAJOR 1
 #define PHMAP_VERSION_MINOR 3
-#define PHMAP_VERSION_PATCH 9
+#define PHMAP_VERSION_PATCH 10
 
 // Included for the __GLIBC__ macro (or similar macros on other systems).
 #include <limits.h>
