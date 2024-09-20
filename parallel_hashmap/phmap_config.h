be87c4139ed827fa80920c89922369701dccd41f
greg7mdp <greg7mdp@gmail.com>
2024-09-20 17:18:43 -0400

    Update version to `1.4.0`

--- parallel_hashmap/phmap_config.h
+++ parallel_hashmap/phmap_config.h
@@ -35,8 +35,8 @@
 // ---------------------------------------------------------------------------
 
 #define PHMAP_VERSION_MAJOR 1
-#define PHMAP_VERSION_MINOR 3
-#define PHMAP_VERSION_PATCH 12
+#define PHMAP_VERSION_MINOR 4
+#define PHMAP_VERSION_PATCH 0
 
 // Included for the __GLIBC__ macro (or similar macros on other systems).
 #include <limits.h>
